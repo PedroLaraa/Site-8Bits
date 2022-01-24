f1e684065b5ff095288389900e6741e91b8d2e71
Pedro Lara <pedroalveslara@gmail.com>
2022-01-24 14:58:00 -0300

    Adição dos arquivos base.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    speed: 300,
+    infinite: true,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+});
